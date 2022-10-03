e46f55a890297bbf5db072dd073f484a1162b132
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-10-03 23:26:35 +0200

    Added iteration for skilltags instead of hardcode

--- src/components/About.js
+++ src/components/About.js
@@ -1,54 +1,30 @@
-const About = () => {
-    return (
-        <div className="container">
-            <div className="about">
-                <div className="picture">
-                <img src="img/me.jpg" alt="" />
-                </div>
-                <div className="about-text">
-                <div className="about-bio">
-                    <h3>Om mig</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum rerum harum, nihil hic fugiat cupiditate recusandae commodi impedit aut eius odio porro sunt, animi nemo nisi laboriosam eaque explicabo aperiam.</p>
-                </div>
-                <div className="about-skills">
-                    <h3>Skills</h3>
-                    <div className="skill-list">
-                    <div className="skill">
-                        <p>C#</p>
-                    </div>
-                    <div className="skill">
-                        <p>.NET</p>
-                    </div>
-                    <div className="skill">
-                        <p>EF</p>
-                    </div>
-                    <div className="skill">
-                        <p>PHP</p>
-                    </div>
-                    <div className="skill">
-                        <p>JS</p>
-                    </div>
-                    <div className="skill">
-                        <p>Node</p>
-                    </div>
-                    <div className="skill">
-                        <p>React</p>
-                    </div>
-                    <div className="skill">
-                        <p>SQL</p>
-                    </div>
-                    <div className="skill">
-                        <p>HTML</p>
-                    </div>
-                    <div className="skill">
-                        <p>CSS</p>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
+import Skill from "../components/Skill";
+
+const About = (props) => {
+  const skills = props.skills;
+  return (
+    <div className="container">
+      <div className="about">
+        <div className="picture">
+          <img src={props.imgUrl} alt="" />
         </div>
-    );
-}
+        <div className="about-text">
+          <div className="about-bio">
+            <h3>Om mig</h3>
+            <p>{props.aboutText}</p>
+          </div>
+          <div className="about-skills">
+            <h3>Skills</h3>
+            <div className="skill-list">
+              {skills.map((skill) => (
+                <Skill title={skill} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
